ee1523540f8840949e91613c35ddaffdf3d6196f
buyallmemes <165269120+buyallmemes@users.noreply.github.com>
2024-03-31 12:43:05 +0200

    feat: jib

--- src/test/java/com/buyallmemes/blogapi/BlogApiApplicationTests.java
+++ src/test/java/com/buyallmemes/blogapi/BlogApiApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
